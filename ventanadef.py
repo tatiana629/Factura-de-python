d7461eae5a01560884bafef9697ba44866f32a82
joak99 <joak99@javerianacali.edu.co>
2017-04-29 15:11:09 -0500

    Add files via upload

---
+++ ventanadef.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+
+
+ventana=Tk()
+ventana.geometry("350x170+200+200")
+ventana.title("acceso")
+usuario=Label(text="Usuario:",fg="blue",font=("Baskerville Old Face",15)).grid(row=0,column=0,sticky=W)
+password=Label(text="Contraseña:", fg="blue",font=("Baskerville Old Face",15)).grid(row=1,column=0)
+entradauser=StringVar()
+
+entradapass=StringVar()
+textoUser=Entry(ventana,textvariable=entradauser,
+                width=30).grid(row=0,column=2)
+textopassword=Entry(ventana,textvariable=entradapass,
+                    show=("*"),width=30).grid(row=1,column=2)
+botoningresar=Button(ventana,text="Ingresar",font=("Cooper Black",12),width=10).place(x=20,y=80)
+botonsalir=Button(ventana,text="salir",font=("Cooper Black",12),width=10,command=quit).place(x=150,y=80)
+ventana.mainloop()
+
+
